b12c598a3aa540f13d257766bf93dccdaf55b122
Shameeka Franklin <shameeka.franklin@gmail.com>
2023-12-09 02:29:00 -0500

    Update Terraform files

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         = "meeka1-terraform-remote-state"
+    bucket         = "meeka1-terraform-rz-state"
     key            = "rentzone-ecs/terraform.tfstate"
     region         = "us-east-1"
     profile        = "Meeka1-terraform"
-    dynamodb_table = "terraform-state-lock"
+    dynamodb_table = "terraform-rz-state-lock"
   }
 }
